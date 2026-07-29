93b0f61aa44a91cc924fe7ac6c87bc59a29000dd
willbrunt <william.brunt1@gmail.com>
2026-07-29 15:53:09 -0600

    Update test.js

--- test.js
+++ test.js
@@ -2,19 +2,20 @@
 var toggle = function(/** @type { Element } */ summary) {
     const details = summary.parentElement;
 
-      frame = details.querySelector("iframe");
-      players = frame.contentWindow.document.body.querySelectorAll("video");
-      console.log("Found " + players.length + " videos!");
-      console.log(players);
-      if (players.length != 0) {
-          players.array.forEach(video => {
-              if (video.paused) {
-                  console.log("playing");
-                  video.play();
-              } else {
-                  console.log("pausing");
-                  video.pause();
-              }
-          });
-      }
+      vid = details.querySelector("iframe");
+      vid.playVideo();
+      // players = frame.contentWindow.document.body.querySelectorAll("video");
+      // console.log("Found " + players.length + " videos!");
+      // console.log(players);
+      // if (players.length != 0) {
+      //     players.array.forEach(video => {
+      //         if (video.paused) {
+      //             console.log("playing");
+      //             video.play();
+      //         } else {
+      //             console.log("pausing");
+      //             video.pause();
+      //         }
+      //     });
+      // }
 };
