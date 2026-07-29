c00c15e10a8f68fae89347e9673b67dea885cdde
willbrunt <william.brunt1@gmail.com>
2026-07-29 15:08:07 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
 
 var toggle = function(/** @type { Element } */ summary) {
     const details = summary.parentElement
-    const lang = details.parentElement.getElementsByClassName("out")[0];
-
-    lang.toggleAttribute("hide")
-    lang.toggleAttribute("shown");
+    
 
       players = details.querySelectorAll("video");
       if (players.length != 0) {
