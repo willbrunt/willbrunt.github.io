ae0bcdf7d3325738e6d277b15494287d12d6f8b3
willbrunt <william.brunt1@gmail.com>
2026-07-29 15:11:15 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,16 @@
 
 var toggle = function(/** @type { Element } */ summary) {
-    const details = summary.parentElement
-    
+    const details = summary.parentElement;
 
       players = details.querySelectorAll("video");
+      console.log("Found " + players.length + " videos!");
       if (players.length != 0) {
           players.array.forEach(video => {
               if (video.paused) {
+                  console.log("playing");
                   video.play();
               } else {
+                  console.log("pausing");
                   video.pause();
               }
           });
